2232ba716c4074734925c7ba358d43b249233b28
dashunzi <406846759@qq.com>
2017-02-03 11:52:24 +0800

    修复一发送请求就在body添加jsonp的bug

--- angular-boilerplate/app/components/auto-focus.js
+++ angular-boilerplate/app/components/auto-focus.js
@@ -4,24 +4,31 @@
 (function (angular) {
 	angular.module('moviecat.directives.auto_focus', [])
 		.directive('autoFocus', ['$location', function ($location) {
-			var path = $location.path();//  /coming_soon/1
+			//var path = $location.path();//  /coming_soon/1
 			return {
 				restrict: 'A',
 				link: function ($scope, iElm, iAttrs, controller) {
-					//获取含有autoFocus指令的a标签的href对应的值
-					var aLink = iElm.children().attr('href');
-					//+一次或多次  ?零次或一次  *任意次 .表示除\n之外的任意字符 在+ ？ *后面加一个？表示懒惰模式
-					var type = aLink.replace(/#(\/.+?)\/\d+/,'$1');
-					if(path.startsWith(type)){
-						iElm.addClass('active');
-					}
-					console.log(type);
-					//点击实现左侧的自动焦点
-					iElm.on('click', function () {
-						//先清除所有的active类
-						iElm.parent().children().removeClass('active');
-						iElm.addClass('active');
+					$scope.$location = $location;
+					//监听$location.path()的变化
+					$scope.$watch('$location.path()', function (now) {
+						//获取含有autoFocus指令的a标签的href对应的值
+						var aLink = iElm.children().attr('href');
+						//+一次或多次  ?零次或一次  *任意次 .表示除\n之外的任意字符 在+ ？ *后面加一个？表示懒惰模式
+						var type = aLink.replace(/#(\/.+?)\/\d+/, '$1');
+						if (now.startsWith(type)) {
+							//先清除所有的active类
+							iElm.parent().children().removeClass('active');
+							iElm.addClass('active');
+						}
 					});
+
+
+					/*	//点击实现左侧的自动焦点
+					 iElm.on('click', function () {
+					 //先清除所有的active类
+					 iElm.parent().children().removeClass('active');
+					 iElm.addClass('active');
+					 });*/
 				}
 			}
 		}]);
--- angular-boilerplate/app/components/http.js
+++ angular-boilerplate/app/components/http.js
@@ -7,14 +7,13 @@
 	// 由于默认angular提供的异步请求对象不支持自定义回调函数名
 	// angular随机分配的回调名称不被豆瓣支持
 	var http = angular.module('moviecat.services.http', []);
-	http.service('HttpService', ['$document','$window', function ($document,$window) {
+	http.service('HttpService', ['$document', '$window', function ($document, $window) {
 		//url :http://api.douban.com/in_theaters  --><script>--html</script>;
 		this.jsonp = function (url, data, callback) {
 			//1.挂载回调函数
 			//函数名my_json_cb_011942038362870733
-			var cbFuncName = 'my_json_cb_'+Math.random().toString().replace('.', '');
-			//$window.my_json_cb_011942038362870733 = callback;
-			$window[cbFuncName] = callback;
+			var cbFuncName = 'my_json_cb_' + Math.random().toString().replace('.', '');
+
 			//2.将data转换为url字符串的形式  {count:6,start:7}-->?count=6&start=7;
 			var querystring = '?';
 			for (var key  in data) {
@@ -27,8 +26,15 @@
 			//4.创建一个script标签
 			var scriptElement = $document[0].createElement('script');
 			scriptElement.src = url + querystring;
-			//注意此时还不能将其append到页面上
 
+			//注意此时还不能将其append到页面上
+			//回调函数
+			//$window.my_json_cb_011942038362870733 = callback;
+			$window[cbFuncName] = function (data) {
+				callback(data);
+				//函数调用之后删除引入的jsonp文件
+				$document[0].body.removeChild(scriptElement);
+			};
 			//5.将script标签放到页面中
 			$document[0].body.appendChild(scriptElement);
 		}
